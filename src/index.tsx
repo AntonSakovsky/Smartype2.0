edb97b550d36a4e0f8e365b7b33d7a4bb4ef2c77
AntonSaky <anton.sakovsky@gmail.com>
2024-07-29 17:32:51 +0300

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <BrowserRouter basename="/smartype-ex">
+    <BrowserRouter basename="/smartype2.0">
         <Provider store={store}>
             <App />
         </Provider>
